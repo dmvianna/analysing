766d0855dc29094fa20fd76eca6673baf41f2695
Daniel Vianna <daniel.vianna@dpc.vic.gov.au>
2017-11-16 16:44:39 +1100

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import           Analyze
+import           Analyze.Csv
+
+
+
+main :: IO ()
+main = do
+
+  -- d <- loadCSVFileWithHeader "../data/pat_abstracts.csv"
+  putStrLn "Hello World"
