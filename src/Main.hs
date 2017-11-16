2ced0d0fa21504fecca5310b1ec630040b7f81bb
Daniel Vianna <daniel.vianna@dpc.vic.gov.au>
2017-11-16 16:57:04 +1100

    takeRows in IO

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,6 @@ import           Analyze.Csv
 
 main :: IO ()
 main = do
+  d <- loadCSVFileWithHeader "../data/pat_abstracts.csv"
+  putStrLn $ show $ takeRows 1 d
 
-  -- d <- loadCSVFileWithHeader "../data/pat_abstracts.csv"
-  putStrLn "Hello World"
